414a8dc5ab45175127aaa0f2d229ee2e44333cf4
QWitcherS <quickwitcher@gmail.com>
2023-12-30 20:25:17 +0500

    Выполнение д/з по уроку React Hooks

--- skypro-kanban/src/App.jsx
+++ skypro-kanban/src/App.jsx
@@ -5,15 +5,38 @@ import PopBrowse from "./components/PopBrowse/PopBrowse";
 import PopExit from "./components/PopExit/PopExit";
 import PopNewCard from "./components/PopNewCard/PopNewCard";
 import Wrapper from "./components/Wrapper/Wrapper";
+import { useEffect, useState } from "react";
+import { cardList } from "./data";
 
 function App() {
+  const [cards, setCards] = useState(cardList);
+  const [isLoaded, setIsLoaded] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(false);
+    }, 1000);
+  }, []);
+
+  function addCard() {
+    setCards([
+      ...cards,
+      {
+        id: cards.length + 1,
+        theme: "Web Design",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "Без статуса",
+      },
+    ]);
+  }
+
   return (
     <Wrapper>
       <PopBrowse />
       <PopNewCard />
       <PopExit />
-      <Header />
-      <Main />
+      <Header addCard={addCard} />
+      <Main isLoaded={isLoaded} cardList={cards} />
     </Wrapper>
   );
 }
